6a8c61e0cfb866ad1cc693f177cf8026c6018655
Max <maksymilianrozanski@gmail.com>
2020-10-11 16:49:21 +0200

    change startup page

--- WebApplication/Startup.cs
+++ WebApplication/Startup.cs
@@ -51,7 +51,7 @@ namespace WebApplication
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Company}/{action=Index}/{id?}");
             });
         }
     }
